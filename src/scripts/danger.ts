71a18178ce3f6d7c4a0514840b72a3b624a21cd2
Ben Darlow <github@kapowaz.net>
2023-08-15 10:52:19 +0100

    feat: add new dangerResults function
    Also updated the dangerReport function to accept platform and scope arguments

--- src/scripts/danger.ts
+++ src/scripts/danger.ts
@@ -1,4 +1,5 @@
 import { reportDisabled } from '../reportDisabled';
+import { GitScope, GrepPlatform, IOverruleReport } from '../types';
 
 const table = ({
   heading,
@@ -15,12 +16,21 @@ const table = ({
 ${rows.map((row) => `|${row.join('|')}|`).join('\n')}
 `;
 
+/**
+ * Generates a report of all disabled directives, as a Markdown table; note that
+ * the platform here defaults to Linux, not BSD, since the assumption is this is
+ * run in CI where Linux is more prevalent.
+ */
 export const dangerReport = async ({
+  platform = GrepPlatform.Linux,
   baseBranch = 'main',
+  scope = GitScope.Branch,
 }: {
+  platform?: GrepPlatform;
+  scope?: GitScope;
   baseBranch?: string;
 }) => {
-  const directives = await reportDisabled({ baseBranch });
+  const directives = await reportDisabled({ platform, baseBranch, scope });
   const heading = 'Lintervention Report';
   const headings = ['Count', 'Rule'];
 
@@ -33,4 +43,22 @@ export const dangerReport = async ({
   const rows = directives.map(({ count, rule }) => [count, rule]);
 
   return table({ heading, headings, rows });
+};
+
+/**
+ * Generates a report of all disabled directives, as an array of IOverruleReport
+ * (i.e. counts keyed by rule name); note that the platform here defaults to
+ * Linux, not BSD, since the assumption is this is run in CI where Linux is more
+ * prevalent.
+ */
+export const dangerResults = async ({
+  platform = GrepPlatform.Linux,
+  baseBranch = 'main',
+  scope = GitScope.Branch,
+}: {
+  platform?: GrepPlatform;
+  scope?: GitScope;
+  baseBranch?: string;
+}): Promise<IOverruleReport[]> => {
+  return await reportDisabled({ platform, baseBranch, scope });
 };
